2d0113d412c6c7c6350534d5633dd32744a1ba50
Tiago Pereira <tiagopereiraduarte8@gmail.com>
2025-02-11 16:42:20 -0300

    Classe Kakashi

---
+++ Tiago/Kakashi.cs
@@ -0,0 +1,9 @@
+﻿namespace ATV_Branch.Tiago
+{
+    public class Kakashi
+    {
+        public string Nome { get; set; }
+        public string Aldeia { get; set; }
+        public string Sensei { get; set; }
+    }
+}
